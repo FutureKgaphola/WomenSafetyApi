dfc869987fd8423f96fccd574afd746bc27eb571
FutureKgaphola <futurekgaphola@gmail.com>
2024-04-05 19:18:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+const checkInternetConnected = require('check-internet-connected');
+const express = require('express'); 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const { sendSMS, sendSMSFalseAlarm } = require("./utils/sendSms");
+
+const app = express(); 
+const PORT = process.env.APP_PORT; 
+app.use(cors());
+app.use(bodyParser.json());
+app.get('/', (req, res)=>{ 
+    res.status(200); 
+    res.send("Welcome to root URL of Server"); 
+}); 
+
+app.post('/sendsms', (req, res)=>{
+  checkInternetConnected()
+  .then((result) => {
+    if(result){
+      const {googleUrl="no message",victimName="Anonymous"} = req.body; 
+      sendSMS(googleUrl,victimName).then(()=>{
+        res.json({"smsdelivery":"succesfully deliverd sms"}); 
+      }).catch((err)=>{
+        res.status(404).json(err);
+      })
+    }
+  }).catch((ex) => {
+    res.status(404).json(ex); 
+  });
+});
+
+app.post('/falsesendsms', (req, res)=>{
+  checkInternetConnected()
+  .then((result) => {
+    if(result){
+      const {victimName="Anonymous"} = req.body; 
+      sendSMSFalseAlarm(victimName).then(()=>{
+        res.json({"smsdelivery":"succesfully deliverd sms"}); 
+      }).catch((err)=>{
+        res.status(404).json(err);
+      })
+    }
+  }).catch((ex) => {
+    res.status(404).json(ex); 
+  });
+});
+
+app.listen(PORT, (error) =>{ 
+	if(!error) 
+		console.log("Server is Successfully Running, and App is listening on port "+ PORT) 
+	else
+		console.log("Error occurred, server can't start", error); 
+	} 
+); 
---
+++ utils/sendSms.js
@@ -0,0 +1,23 @@
+const { Vonage } = require('@vonage/server-sdk')
+
+const vonage = new Vonage({
+  apiKey: process.env.SMSAPIKEY,
+  apiSecret: process.env.SMSAPISECRET
+})
+
+const from = "WomenSafety"
+const to = "27738285578"
+
+module.exports.sendSMS=async(googleUrl,victimName)=> {
+    const text = `WomenSafety.\n Hi your someone close to you (${String(victimName)}) apears to be in danger. Click the link to located them: - ${String(googleUrl)}.\n\n.`;
+    await vonage.sms.send({to, from, text})
+        .then(resp => { console.log('Message sent successfully'); console.log(resp); })
+        .catch(err => { console.log('There was an error sending the messages.'); console.error(err); });
+}
+
+module.exports.sendSMSFalseAlarm=async(victimName)=> {
+  const text = `WomenSafety.\n (${String(victimName)}) has reported the recent location as a false alarm.\n\n.`;
+  await vonage.sms.send({to, from, text})
+      .then(resp => { console.log('Message sent successfully'); console.log(resp); })
+      .catch(err => { console.log('There was an error sending the messages.'); console.error(err); });
+}
